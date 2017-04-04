b6f60f69293242f1312dfb5abdcf0766e44ee912
Zach Lee <zach3lee@gmail.com>
2017-04-03 19:54:15 -0700

    added a reroll mechanism to the unigram songwriter because it showed up more often than I preferred

--- models/random_models.py
+++ models/random_models.py
@@ -15,6 +15,17 @@ MODELS = {
 def get_models(artist):
 	'Select two random models and return their names with their objects'
 	model1_name, model2_name  = random.sample(MODELS.keys(), 2)
+
+	# reroll unigram songwriter 60% of the time because it sucks
+	if model1_name == 'GoogleUnigramSongWriter':
+		roll = random.randint(1, 10)
+		if roll > 4:
+			model1_name = random.choice(list(MODELS.keys()))
+	if model2_name == 'GoogleUnigramSongWriter':
+		roll = random.randint(1, 10)
+		if roll > 4:
+			model2_name = random.choice(list(MODELS.keys()))
+
 	# initialize models
 	model1 = MODELS[model1_name](artist)
 	model2 = MODELS[model2_name](artist)
